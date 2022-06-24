5c8768647b9bb0f23b6978928a38135fc7b3959e
Liam Nally <nallyl@wit.edu>
2022-06-24 19:53:35 -0400

    Rightbar component layout/style

--- app-react/src/components/rightbar/Rightbar.jsx
+++ app-react/src/components/rightbar/Rightbar.jsx
@@ -3,6 +3,25 @@ import './rightbar.css'
 
 export default function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/person/default.jpeg" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>You</b> and <b>2 other people</b> have a birthday today.
+          </span>
+        </div>
+        <h4 className="rightbarFriendList">Online friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/default.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Friend Username</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
