e706fff4e32cd1d18624a651e85b4aa30ddd42cb
Liam Nally <nallyl@wit.edu>
2022-07-21 12:16:46 -0400

    Logged out user now redirected to Login page

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Route exact path="/" element={user ? <Home /> : <Register/>}/>
       <Route path="/login" element={user ? <Navigate to="/" /> : <Login />}/>
       <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
-      <Route path="/profile/:username" element={<Profile />}/>
+      <Route path="/profile/:username" element={user ? <Profile /> : <Login />}/>
     </Routes>
    </Router>
   );
